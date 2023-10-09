3a7e4b655c8892741e7206723697672ce0891f90
ychilo <seojunchian@gmail.com>
2023-10-09 23:20:39 +0300

    asd

---
+++ script.js
@@ -0,0 +1,61 @@
+/* SELECTION */
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const divide = document.querySelector("#divide");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const multiplication = document.querySelector("#multiplication");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const extraction = document.querySelector("#extraction");
+const zero = document.querySelector("#zero");
+const addition = document.querySelector("#addition");
+const screen = document.querySelector("#screen");
+const result = document.querySelector("#result");
+
+/* EVENT LISTENING */
+seven.addEventListener("click", () => {
+    result.textContent = 7;
+})
+eight.addEventListener("click", () => {
+
+})
+nine.addEventListener("click", () => {
+
+})
+divide.addEventListener("click", () => {
+
+})
+four.addEventListener("click", () => {
+
+})
+five.addEventListener("click", () => {
+
+})
+six.addEventListener("click", () => {
+
+})
+multiplication.addEventListener("click", () => {
+
+})
+one.addEventListener("click", () => {
+
+})
+two.addEventListener("click", () => {
+
+})
+three.addEventListener("click", () => {
+
+})
+extraction.addEventListener("click", () => {
+
+})
+zero.addEventListener("click", () => {
+
+})
+addition.addEventListener("click", () => {
+
+})
